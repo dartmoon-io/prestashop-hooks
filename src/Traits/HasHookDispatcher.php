59b21c781fc27fb4f297741eb9aa1177382bdd11
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2021-06-16 15:25:26 +0200

    Added HasHookDispatcher trait

---
+++ src/Traits/HasHookDispatcher.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Dartmoon\Hooks\Traits;
+
+use Dartmoon\Hooks\HookDispatcher;
+
+trait HasHookDispatcher
+{
+    /**
+     * Hook dispatcher
+     */
+    protected $hookDispatcher;
+
+    /**
+     * Init the hook dispatcher
+     */
+    protected function initHookDispatcher()
+    {
+        $this->hookDispatcher = new HookDispatcher();
+
+        // Let's check if the module define the
+        // hook property
+        if (!isset($this->hooks)) {
+            return;
+        }
+
+        // Register all hooks found
+        foreach ($this->hooks as $hookClass) {
+            $this->hookDispatcher->register(new $hookClass($this));
+        }
+    }
+
+    /**
+     * Return the hook dispatcher
+     */
+    protected function getHookDispatcher()
+    {
+        return $this->hookDispatcher;
+    }
+
+    /**
+     * Dispatch hooks
+     *
+     * @param string $methodName
+     * @param array $arguments
+     */
+    public function __call($methodName, array $arguments)
+    {
+        $hookName = preg_replace('~^hook~', '', $methodName);
+        return $this->hookDispatcher->dispatch(
+            $hookName,
+            !empty($arguments[0]) ? $arguments[0] : []
+        );
+    }
+}
